3d1f47db826f76d463f9c335eea31e7bae8cf1c0
Aarni Junkkala <83189693+Datafiili@users.noreply.github.com>
2023-09-27 16:54:59 +0300

    Add files via upload

---
+++ pngtestaus.py
@@ -0,0 +1,12 @@
+import math
+pieces = 8
+scores = []
+Ascores = []
+for i in range(pieces):
+    scores.append(math.sin(math.pi / 2 / pieces * i))
+    print(scores[i])
+print("---------------------------")
+
+for i in range(len(scores)):
+    Ascores.append(math.asin(scores[i]))
+    print(Ascores[i])
